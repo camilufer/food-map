8f8bea29ea35128005444ecf9b4adba2b467167b
camilufer <camilu.fer21@gmail.com>
2017-12-12 22:51:47 -0300

    primer commit

---
+++ assets/js/splash.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+setTimeout(function() {
+    $('.desaparece').fadeOut('fast');
+}, 5000); 
+
+
+setTimeout(function() {
+	$(location).attr('href','index.html');  
+}, 5000);
+
+
+});
+
